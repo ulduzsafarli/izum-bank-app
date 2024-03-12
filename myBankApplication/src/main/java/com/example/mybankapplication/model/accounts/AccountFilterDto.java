bfcd235b649202e0707d5c5b53a5d72237dc1b55
Ulduz Safarli <ulduzsafarli.dv@gmail.com>
2024-03-12 18:48:26 +0400

    #5 change UserResponse.java: remove password and add UserProfileDto.java

--- myBankApplication/src/main/java/com/example/mybankapplication/model/accounts/AccountFilterDto.java
+++ myBankApplication/src/main/java/com/example/mybankapplication/model/accounts/AccountFilterDto.java
@@ -3,7 +3,6 @@ package com.example.mybankapplication.model.accounts;
 import com.example.mybankapplication.enumeration.accounts.AccountStatus;
 import com.example.mybankapplication.enumeration.accounts.AccountType;
 import com.example.mybankapplication.enumeration.accounts.CurrencyType;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,7 +16,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccountFilterDto {
-    @Size(min = 0, max = 3, message = "Branch code must contain 3 digits")
+//    @Size(max = 3, message = "Branch code must contain 3 digits")
     private String branchCode;
     private LocalDate accountOpenDate;
     private LocalDate accountExpireDate;
--- myBankApplication/src/main/java/com/example/mybankapplication/model/users/UserResponse.java
+++ myBankApplication/src/main/java/com/example/mybankapplication/model/users/UserResponse.java
@@ -2,16 +2,9 @@ package com.example.mybankapplication.model.users;
 
 import com.example.mybankapplication.enumeration.auth.Role;
 import com.example.mybankapplication.model.users.profile.UserProfileDto;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-import java.util.Collections;
-import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
