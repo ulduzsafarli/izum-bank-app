f3a34bfb977abad3cba1bd217a99e57e518b7ad8
Ulduz Safarli <ulduzsafarli.dv@gmail.com>
2024-04-21 01:07:23 +0400

    #47 solve Detached Entity Passed to Persist by using Cascade.Merge

--- src/main/java/org/matrix/izumbankapp/service/impl/DepositServiceImpl.java
+++ src/main/java/org/matrix/izumbankapp/service/impl/DepositServiceImpl.java
@@ -3,18 +3,31 @@ package org.matrix.izumbankapp.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.matrix.izumbankapp.dao.repository.DepositRepository;
+import org.matrix.izumbankapp.enumeration.accounts.AccountType;
 import org.matrix.izumbankapp.exception.NotFoundException;
 import org.matrix.izumbankapp.mapper.DepositMapper;
+import org.matrix.izumbankapp.model.accounts.AccountCreateDto;
+import org.matrix.izumbankapp.model.accounts.AccountResponse;
+import org.matrix.izumbankapp.model.deposits.DepositRequest;
 import org.matrix.izumbankapp.model.deposits.DepositResponse;
+import org.matrix.izumbankapp.service.AccountService;
 import org.matrix.izumbankapp.service.DepositService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class DepositServiceImpl implements DepositService {
+    private final PasswordEncoder passwordEncoder;
+    private final AccountService accountService;
     private final DepositRepository depositRepository;
     private final DepositMapper depositMapper;
 
@@ -23,7 +36,7 @@ public class DepositServiceImpl implements DepositService {
         log.info("Saving deposit");
         var deposit = depositMapper.toEntity(depositResponse);
         var account = depositRepository.findById(deposit.getId())
-                .orElseThrow(()-> new NotFoundException("Account not found for deposit ID" + deposit.getId()))
+                .orElseThrow(() -> new NotFoundException("Account not found for deposit ID" + deposit.getId()))
                 .getAccount();
         account.setCurrentBalance(deposit.getAccount().getCurrentBalance());
         deposit.setAccount(account);
@@ -40,6 +53,46 @@ public class DepositServiceImpl implements DepositService {
         return deposits;
     }
 
+    @Override
+    @Transactional
+    public void create(DepositRequest depositRequest) {
+        log.info("Creating deposit account for user: {}", depositRequest.getUserId());
 
+        AccountCreateDto accountDto = new AccountCreateDto(
+                depositRequest.getUserId(),
+                "333",
+                depositRequest.getDepositExpireDate(),
+                depositRequest.getCurrencyType(),
+                AccountType.DEPOSIT,
+                calculateInterest(depositRequest.getAmount(), depositRequest.getInterest(), depositRequest.getDepositExpireDate()),
+                BigDecimal.ZERO,
+                null,
+                passwordEncoder.encode(depositRequest.getPin())
+        );
+
+        AccountResponse accountResponse = accountService.create(accountDto);
+
+        DepositResponse depositResponse = DepositResponse.builder()
+                .account(accountService.getById(accountResponse.getId()))
+                .amount(depositRequest.getAmount())
+                .interestRate(depositRequest.getInterest())
+                .yearlyInterest(depositRequest.isYearlyInterest()).build();
+
+        depositRepository.save(depositMapper.toEntity(depositResponse));
+        log.info("Deposit account created successfully");
+    }
+
+    private BigDecimal calculateInterest(BigDecimal amount, BigDecimal interest, LocalDate depositExpireDate) {
+        LocalDate currentDate = LocalDate.now();
+        Period period = Period.between(currentDate, depositExpireDate);
+        int months = period.getMonths() + period.getYears() * 12;
+
+        BigDecimal interestRate = BigDecimal.ZERO.add(interest.divide(BigDecimal.valueOf(100),
+                2, RoundingMode.HALF_UP));
+        BigDecimal monthlyAmount = amount.multiply(interestRate);
+        BigDecimal calculatedInterest = monthlyAmount.multiply(BigDecimal.valueOf(months));
+
+        return amount.add(calculatedInterest);
+    }
 
 }
